16c9161ce00da14565a2c9b5834aa3b357402b50
walterwj <277372664@qq.com>
2020-10-22 02:17:29 +0800

    master

--- Script/run_tidb.py
+++ Script/run_tidb.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+# System modules
+from threading import Thread
+import time
 import mysql.connector
 import argparse
-import time
-import os
-import threading
-
-from multiprocessing import Process
-
-try:
-    import Queue
-except:
-    import queue
-
 ## If Python is version 2.7, encoding problems can reload sys configuration
 try:
     import sys
@@ -23,3 +15,66 @@ try:
 except:
     pass
 
+def mysql_exe():
+    args = parser_args()
+    try:
+        connection = mysql.connector.connect(host=args.host,
+                                 user=args.user,
+                                 password=args.password,
+                                 port=args.port,
+                                 charset='utf8mb4',
+                                 database=args.database)
+    except all as error:
+        print("error is ", error)
+    finally:
+        pass
+
+    try:
+        _sql = open("test.sql","r+").read()
+        cursor = connection.cursor()
+        cursor.execute("SET NAMES utf8mb4")
+        cursor.execute(_sql)
+        print("insert into sucessfull~")
+        cursor.execute('delete from t3 limit 1')
+        print("delete sucessfull~")
+        cursor.close()
+        connection.commit()
+    except all as error:
+        print("error is ", error)
+    finally:
+        connection.close()
+    a = 'mysql'
+    return a
+
+def main():
+    args = parser_args()
+    threads = []
+    for i in range(args.thread_num):
+        threads.append(Thread(target=mysql_exe,))
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+
+def parser_args():
+    parser = argparse.ArgumentParser(description="Test case for cq")
+    parser.add_argument("--host", dest="host", help="TiDB's host",default='127.0.0.1')
+    parser.add_argument("-P", dest="port", help="TiDB's port",default=4000)
+    parser.add_argument("-u", dest="user", help="TiDB's user",default="root")
+    parser.add_argument("-p",dest="password", help="TiDB's passwd",default='')
+    parser.add_argument("-b",dest="database",help="TiDB's database", default="test")
+    parser.add_argument("--thread",dest="thread_num",help="execute thread",default=5)
+    parser.add_argument("--exe",dest="range_num",help="Execution times",default=100)
+    args = parser.parse_args()
+
+    return args
+
+if __name__ == "__main__":
+    args = parser_args()
+    range_num, thread_num = args.range_num, args.thread_num
+    st = time.time()
+    for i in range(range_num/thread_num):
+        main()
+    ct = time.time() - st
+    print("total cost time: {}s").format(ct)
+    print("avg is: {}s").format(ct/range_num)
